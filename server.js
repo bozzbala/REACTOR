20c96f8bbae5437e360a385c5cfb2339d90f63e8
Temirlan <flashtima@gmail.com>
2022-06-07 19:44:19 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const createPath = (page) => path.resolve(__dirname, `${page}.html`)
+
+app.listen(3000, (error) =>{
+    if(error){
+        throw error;
+    }
+    else{
+        console.log(`listening to port ${3000}`);
+    }
+})
+
+app.get("/", (req, res) => {
+    
+})
